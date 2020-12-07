3a9459d207dcd9030e5f54421273577f506ecfef
AbdelRaouf Dannaoui <123raoufdannaoui@gmail.com>
2020-12-07 07:06:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,3 +21,18 @@ function wrapAdjective(par1="*")//1
   }
 }
 wrapAdjective("%")("a dedicated programmer");
+
+const Calculator = {
+  function add(a,b){
+    return a+b;
+  },
+  function subtract(a,b){
+    return a-b;
+  },
+  function multiply(a,b){
+    return a*b;
+  },
+  function divide(a,b){
+    return a/b;
+  },
+}

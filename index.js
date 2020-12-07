b86d26cf6323458a61d851526340ff4e9a0de7e5
AbdelRaouf Dannaoui <123raoufdannaoui@gmail.com>
2020-12-07 06:59:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Your code here
 function saturdayFun(par){
-  if (typeof(par)==='undefined') par = "roller-skate";
-
+if (typeof(par)==='undefined') par = "roller-skate";
 return `This Saturday, I want to ${par}!`
 }
 saturdayFun();
@@ -9,9 +8,16 @@ saturdayFun("eat");
 
 
 function mondayWork(par = "go to the office"){
-
-
 return `This Monday, I will ${par}.`
 }
-
 saturdayFun("eat");
+
+
+function wrapAdjective(par1="*")//1
+{
+  return function(par="special") //2
+  {
+    return `You are ${par1}`
+  }
+}
+wrapAdjective("%")("a dedicated programmer")
